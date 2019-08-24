7544132360bed4fefef81e6c777835e62df22b25
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-08-25 01:14:34 +0900

    update

--- src/Main.elm
+++ src/Main.elm
@@ -1,61 +1,130 @@
 module Main exposing (..)
 
+import Browser
+import Color.OneDark as OneDark
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
-import OneDark exposing (..)
+import Element.Region as Region
+import Html exposing (Html)
 
 
 
 --- DATA
 
 
-type alias Post =
-    { title : String
-    , link : String
-    }
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
 
 
-posts : List Post
-posts =
-    [ { title = "First"
-      , link = "./first"
-      }
-    , { title = "Second"
-      , link = "./second"
-      }
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+type Msg
+    = None
+
+
+update : Msg -> Model -> Model
+update msg model =
+    {}
+
+
+view : Model -> Html Msg
+view model =
+    Element.layout
+        layoutOption
+    <|
+        Element.row
+            [ width fill
+            , height fill
+            ]
+            [ project, viewer ]
+
+
+darkBlack : Element.Color
+darkBlack =
+    rgb255 14 21 26
+
+
+layoutOption : List (Attribute Msg)
+layoutOption =
+    [ Background.color darkBlack
+    , Font.color OneDark.white
+    , Font.regular
+    , Font.size 32
+    , Font.family
+        [ Font.external
+            { url = "https://fonts.googleapis.com/css?family=Droid+Sans+Mono"
+            , name = "Droid Sans Mono"
+            }
+        , Font.sansSerif
+        ]
     ]
 
 
-main =
-    Element.layout
-        [ Background.color OneDark.black
-        , Font.color OneDark.white
-        , Font.regular
-        , Font.size 32
-        , Font.family
-            [ Font.external
-                { url = "https://fonts.googleapis.com/css?family=Droid+Sans+Mono"
-                , name = "Droid Sans Mono"
-                }
-            , Font.sansSerif
-            ]
+project : Element Msg
+project =
+    Element.column
+        [ width (fillPortion 20)
+        , spacing 36
+        , padding 10
+        , Border.color (Element.rgb255 0 0 0)
+        , explain Debug.todo
         ]
-    <|
-        Element.table
-            [ Element.centerX
-            , Element.centerY
-            , Element.spacing 5
-            , Element.padding 10
+        [ projectHeader
+        , el
+            [ Font.size 14
+            , centerX
             ]
-            { data = posts
-            , columns =
-                [ { header = Element.text "TITLE"
-                  , width = px 200
-                  , view =
-                        \post ->
-                            Element.text post.title
-                  }
-                ]
-            }
+            (Element.text "content")
+        ]
+
+
+projectHeader : Element Msg
+projectHeader =
+    el
+        [ Font.size 16
+        , Region.heading 1
+        , height (px 35)
+        , centerX
+        ]
+        (Element.text "Project")
+
+
+viewerHeader : Element Msg
+viewerHeader =
+    el
+        [ Font.size 16
+        , Region.heading 1
+        , height (px 35)
+        ]
+        (Element.text "CAPPYZAWA")
+
+
+viewer : Element Msg
+viewer =
+    Element.column
+        [ width (fillPortion 80)
+        , spacing 36
+        , padding 10
+        , Background.color OneDark.black
+        , explain Debug.todo
+        ]
+        [ viewerHeader
+        , el
+            [ Font.size 14
+            ]
+            (Element.text "content")
+        ]
